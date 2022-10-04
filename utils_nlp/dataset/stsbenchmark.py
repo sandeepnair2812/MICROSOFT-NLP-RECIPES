1cb6b411271615fe3f45e37a0eb296e9ff1cb3fc
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 03:08:31 +0000

    Adding numeric_owner as keyword arguement

--- utils_nlp/dataset/stsbenchmark.py
+++ utils_nlp/dataset/stsbenchmark.py
@@ -91,7 +91,7 @@ def _extract_sts(tarpath, target_dirpath=".", tmode="r"):
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(t, target_dirpath)
--- utils_nlp/dataset/url_utils.py
+++ utils_nlp/dataset/url_utils.py
@@ -118,7 +118,7 @@ def extract_tar(file_path, dest_path="."):
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(t, path=dest_path)
